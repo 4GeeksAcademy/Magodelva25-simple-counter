6f40d2b95dc8552b5f22be7c196ea0ef86090a43
Mangodev <migudz@gmail.com>
2024-11-11 11:46:12 +0000

    functional version

---
+++ src/js/component/Clock.jsx
@@ -0,0 +1,69 @@
+import { element } from 'prop-types';
+import React, { useRef, useEffect } from 'react';
+
+export function Clock() {
+
+  const unidadesRef = useRef(0);
+  const decenasRef = useRef(0);
+  const centenasRef = useRef(0);
+  const milesRef = useRef(0);
+  const decenasMilesRef = useRef(0);
+  const centenasMilesRef = useRef(0);
+  const [,setRender] = React.useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      unidadesRef.current += 1;
+
+      if (unidadesRef.current === 10) {
+        unidadesRef.current = 0;
+        decenasRef.current += 1;
+      }
+      if (decenasRef.current === 100) {
+        decenasRef.current = 0;
+        centenasRef.current += 1;
+      }
+      if (centenasRef.current === 1000) {
+        centenasRef.current = 0;
+        milesRef.current += 1;
+      }
+
+      if (milesRef.current === 10000) {
+        milesRef.current = 0;
+        decenasMilesRef.current += 1;
+      }
+
+      if (decenasMilesRef.current === 100000) {
+        decenasMilesRef.current = 0;
+        centenasMilesRef.current += 1;
+      }
+
+      setRender((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (time) => String(time).padStart(1, '0');
+  
+  const unidades = formatTime(unidadesRef.current);
+  const decenas = formatTime(decenasRef.current);
+  const centenas = formatTime(centenasRef.current);
+  const miles = formatTime(milesRef.current);
+  const decenasMiles = formatTime(decenasMilesRef.current);
+  const centenasMiles = formatTime(centenasMilesRef.current);
+
+
+  return (
+    <div className='wrapper'>
+      <div className='clock'>
+          <p>{centenasMiles}</p>
+          <p>{decenasMiles}</p>
+          <p>{miles}</p>
+          <p>{centenas}</p>
+          <p>{decenas}</p>
+          <p>{unidades}</p>
+      </div>
+    </div>
+  );
+}
